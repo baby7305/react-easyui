779e929ecc473f4a41cbf98778604f027402175f
baby7305 <songhaibei7305@163.com>
2018-12-13 11:48:56 +0800

    路由初始化

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
+import About from "./home/About";
+import Index from "./home/Home";
+import Users from "./home/Users";
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello, world!</h1>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about/">About</Link>
+            </li>
+            <li>
+              <Link to="/users/">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route path="/" exact component={Index} />
+        <Route path="/about/" component={About} />
+        <Route path="/users/" component={Users} />
+      </div>
     );
   }
 }
