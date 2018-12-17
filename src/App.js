fdc5d34573bff7fde37eb74e1433cc7e141438bc
baby7305 <songhaibei7305@163.com>
2018-12-17 14:48:05 +0800

    password-box demo

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import About from "./home/About";
 import Index from "./home/Home";
 import Users from "./home/Users";
 
+import PasswordBoxBasic from "./password-box/Basic";
+
 class App extends Component {
   render() {
     return (
@@ -21,11 +23,19 @@ class App extends Component {
               <Link to="/users/">Users</Link>
             </li>
           </ul>
+          <h2>PasswordBox content</h2>
+          <ul>
+            <li>
+              <Link to="/password-box/basic">basic</Link>
+            </li>
+          </ul>
         </nav>
 
         <Route path="/" exact component={Index} />
         <Route path="/about/" component={About} />
         <Route path="/users/" component={Users} />
+        <h2>PasswordBox content</h2>
+        <Route path="/password-box/basic" component={PasswordBoxBasic} />
       </div>
     );
   }
